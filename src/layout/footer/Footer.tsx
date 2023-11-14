28f68b07b3f8fdba0738226a34f203b08c050321
maksimselchukou <selchukouwebdev1995@gmail.com>
2023-11-14 16:23:43 +0300

    feat:refactor section Footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,89 +1,36 @@
 import React from 'react';
-import styled from "styled-components";
 import {Icon} from "../../components/icon/Icon";
 import {FlexWrapper} from "../../components/FlexWrapper";
-import {theme} from '../../styles/Theme';
+import {S} from './Footer_Styles'
 
 export const Footer = () => {
+
+    const SocialItemsData = [
+        {iconID: 'instagram'},
+        {iconID: 'telegram'},
+        {iconID: 'vk'},
+        {iconID: 'linkedin'},
+    ]
+
     return (
-        <StyledFooter>
+        <S.Footer>
             <FlexWrapper align={'center'} direction={'column'}>
-                <Name>Maxim</Name>
-                <SocialList>
-                    <SocialItem>
-                        <SocialIconLink>
-                            <Icon height={'21px'} width={'21px'} viewBox={'0 0 21px 21px'} iconID={'instagram'}/>
-                        </SocialIconLink>
-                    </SocialItem>
-                    <SocialItem>
-                        <SocialIconLink>
-                            <Icon height={'21px'} width={'21px'} viewBox={'0 0 21px 21px'} iconID={'telegram'}/>
-                        </SocialIconLink>
-                    </SocialItem>
-                    <SocialItem>
-                        <SocialIconLink>
-                            <Icon height={'21px'} width={'21px'} viewBox={'0 0 21px 21px'} iconID={'vk'}/>
-                        </SocialIconLink>
-                    </SocialItem>
-                    <SocialItem>
-                        <SocialIconLink>
-                            <Icon height={'21px'} width={'21px'} viewBox={'0 0 21px 21px'} iconID={'linkedin'}/>
-                        </SocialIconLink>
-                    </SocialItem>
-                </SocialList>
-                <Copyright>© 2023, All Rights Reserved.</Copyright>
+                <S.Name>Maxim</S.Name>
+                <S.SocialList>
+                    {SocialItemsData.map((i, ind) => {
+                        return (
+                            <li key={ind}>
+                                <S.SocialIconLink>
+                                    <Icon height={'21px'} width={'21px'} viewBox={'0 0 21px 21px'} iconID={i.iconID}/>
+                                </S.SocialIconLink>
+                            </li>
+                        )
+                    })}
+                </S.SocialList>
+                <S.Copyright>© 2023, All Rights Reserved.</S.Copyright>
             </FlexWrapper>
-        </StyledFooter>
+        </S.Footer>
     );
 };
 
 
-const StyledFooter = styled.footer`
-  background-color: ${theme.color.primaryBg};
-  padding: 40px 0;
-`
-
-const Name = styled.span`
-  font-family: Josefin Sans, sans-serif;
-  font-size: 22px;
-  font-weight: 700;
-  letter-spacing: 3px;
-  @media ${theme.media.mobile} {
-    font-size: 16px
-  }
-`
-
-const SocialList = styled.ul`
-  display: flex;
-  gap: 20px;
-  margin: 30px 0;
-`
-
-const SocialItem = styled.li`
-`
-
-
-const SocialIconLink = styled.a`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 35px;
-  height: 35px;
-  border-radius: 50%;
-  background-color: rgba(255, 255, 255, 0.10);
-  color: ${theme.color.accent};
-
-  &:hover {
-    position: relative;
-    top: -30%;
-    color: ${theme.color.primaryBg};
-    background-color: ${theme.color.accent};
-  }
-
-`
-
-const Copyright = styled.small`
-  color: rgba(255, 255, 255, 0.50);
-  font-size: 12px;
-  font-weight: 400;
-`
---
+++ src/layout/footer/Footer_Styles.ts
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import {theme} from "../../styles/Theme";
+
+const Footer = styled.footer`
+  background-color: ${theme.color.primaryBg};
+  padding: 40px 0;
+`
+
+const Name = styled.span`
+  font-family: Josefin Sans, sans-serif;
+  font-size: 22px;
+  font-weight: 700;
+  letter-spacing: 3px;
+  @media ${theme.media.mobile} {
+    font-size: 16px
+  }
+`
+
+const SocialList = styled.ul`
+  display: flex;
+  gap: 20px;
+  margin: 30px 0;
+`
+
+
+const SocialIconLink = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  background-color: rgba(255, 255, 255, 0.10);
+  color: ${theme.color.accent};
+
+  &:hover {
+    position: relative;
+    top: -30%;
+    color: ${theme.color.primaryBg};
+    background-color: ${theme.color.accent};
+  }
+
+`
+
+const Copyright = styled.small`
+  color: rgba(255, 255, 255, 0.50);
+  font-size: 12px;
+  font-weight: 400;
+`
+
+export const S = {
+    Footer, Name, SocialList, SocialIconLink, Copyright
+}
